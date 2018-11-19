f8eb9e5ecda59e0c367a66660d26731983a2ce98
Rubens Rech <rubensch@icloud.com>
2018-11-19 14:11:53 -0200

    Change deconvolutional_layer

--- src/deconvolutional_layer.c
+++ src/deconvolutional_layer.c
@@ -12,17 +12,17 @@
 
 
 static size_t get_workspace_size(layer l){
-    return (size_t)l.h*l.w*l.size*l.size*l.n*sizeof(float);
+    return (size_t)l.h*l.w*l.size*l.size*l.n*sizeof(real);
 }
 
 void bilinear_init(layer l)
 {
     int i,j,f;
-    float center = (l.size-1) / 2.;
+    real center = (l.size-1) / 2.;
     for(f = 0; f < l.n; ++f){
         for(j = 0; j < l.size; ++j){
             for(i = 0; i < l.size; ++i){
-                float val = (1 - fabs(i - center)) * (1 - fabs(j - center));
+                real val = (1 - fabs(i - center)) * (1 - fabs(j - center));
                 int c = f%l.c;
                 int ind = f*l.size*l.size*l.c + c*l.size*l.size + j*l.size + i;
                 l.weights[ind] = val;
@@ -49,14 +49,14 @@ layer make_deconvolutional_layer(int batch, int h, int w, int c, int n, int size
     l.nweights = c*n*size*size;
     l.nbiases = n;
 
-    l.weights = calloc(c*n*size*size, sizeof(float));
-    l.weight_updates = calloc(c*n*size*size, sizeof(float));
+    l.weights = calloc(c*n*size*size, sizeof(real));
+    l.weight_updates = calloc(c*n*size*size, sizeof(real));
 
-    l.biases = calloc(n, sizeof(float));
-    l.bias_updates = calloc(n, sizeof(float));
-    //float scale = n/(size*size*c);
+    l.biases = calloc(n, sizeof(real));
+    l.bias_updates = calloc(n, sizeof(real));
+    //real scale = n/(size*size*c);
     //printf("scale: %f\n", scale);
-    float scale = .02;
+    real scale = .02;
     for(i = 0; i < c*n*size*size; ++i) l.weights[i] = scale*rand_normal();
     //bilinear_init(l);
     for(i = 0; i < n; ++i){
@@ -70,10 +70,10 @@ layer make_deconvolutional_layer(int batch, int h, int w, int c, int n, int size
     l.outputs = l.out_w * l.out_h * l.out_c;
     l.inputs = l.w * l.h * l.c;
 
-    scal_cpu(l.nweights, (float)l.out_w*l.out_h/(l.w*l.h), l.weights, 1);
+    scal_cpu(l.nweights, (real)l.out_w*l.out_h/(l.w*l.h), l.weights, 1);
 
-    l.output = calloc(l.batch*l.outputs, sizeof(float));
-    l.delta  = calloc(l.batch*l.outputs, sizeof(float));
+    l.output = calloc(l.batch*l.outputs, sizeof(real));
+    l.delta  = calloc(l.batch*l.outputs, sizeof(real));
 
     l.forward = forward_deconvolutional_layer;
     l.backward = backward_deconvolutional_layer;
@@ -82,30 +82,30 @@ layer make_deconvolutional_layer(int batch, int h, int w, int c, int n, int size
     l.batch_normalize = batch_normalize;
 
     if(batch_normalize){
-        l.scales = calloc(n, sizeof(float));
-        l.scale_updates = calloc(n, sizeof(float));
+        l.scales = calloc(n, sizeof(real));
+        l.scale_updates = calloc(n, sizeof(real));
         for(i = 0; i < n; ++i){
             l.scales[i] = 1;
         }
 
-        l.mean = calloc(n, sizeof(float));
-        l.variance = calloc(n, sizeof(float));
+        l.mean = calloc(n, sizeof(real));
+        l.variance = calloc(n, sizeof(real));
 
-        l.mean_delta = calloc(n, sizeof(float));
-        l.variance_delta = calloc(n, sizeof(float));
+        l.mean_delta = calloc(n, sizeof(real));
+        l.variance_delta = calloc(n, sizeof(real));
 
-        l.rolling_mean = calloc(n, sizeof(float));
-        l.rolling_variance = calloc(n, sizeof(float));
-        l.x = calloc(l.batch*l.outputs, sizeof(float));
-        l.x_norm = calloc(l.batch*l.outputs, sizeof(float));
+        l.rolling_mean = calloc(n, sizeof(real));
+        l.rolling_variance = calloc(n, sizeof(real));
+        l.x = calloc(l.batch*l.outputs, sizeof(real));
+        l.x_norm = calloc(l.batch*l.outputs, sizeof(real));
     }
     if(adam){
-        l.m = calloc(c*n*size*size, sizeof(float));
-        l.v = calloc(c*n*size*size, sizeof(float));
-        l.bias_m = calloc(n, sizeof(float));
-        l.scale_m = calloc(n, sizeof(float));
-        l.bias_v = calloc(n, sizeof(float));
-        l.scale_v = calloc(n, sizeof(float));
+        l.m = calloc(c*n*size*size, sizeof(real));
+        l.v = calloc(c*n*size*size, sizeof(real));
+        l.bias_m = calloc(n, sizeof(real));
+        l.scale_m = calloc(n, sizeof(real));
+        l.bias_v = calloc(n, sizeof(real));
+        l.scale_v = calloc(n, sizeof(real));
     }
 
 #ifdef GPU
@@ -169,7 +169,7 @@ void denormalize_deconvolutional_layer(layer l)
 {
     int i, j;
     for(i = 0; i < l.n; ++i){
-        float scale = l.scales[i]/sqrt(l.rolling_variance[i] + .00001);
+        real scale = l.scales[i]/sqrt(l.rolling_variance[i] + .00001);
         for(j = 0; j < l.c*l.size*l.size; ++j){
             l.weights[i*l.c*l.size*l.size + j] *= scale;
         }
@@ -190,11 +190,11 @@ void resize_deconvolutional_layer(layer *l, int h, int w)
     l->outputs = l->out_h * l->out_w * l->out_c;
     l->inputs = l->w * l->h * l->c;
 
-    l->output = realloc(l->output, l->batch*l->outputs*sizeof(float));
-    l->delta  = realloc(l->delta,  l->batch*l->outputs*sizeof(float));
+    l->output = realloc(l->output, l->batch*l->outputs*sizeof(real));
+    l->delta  = realloc(l->delta,  l->batch*l->outputs*sizeof(real));
     if(l->batch_normalize){
-        l->x = realloc(l->x, l->batch*l->outputs*sizeof(float));
-        l->x_norm  = realloc(l->x_norm, l->batch*l->outputs*sizeof(float));
+        l->x = realloc(l->x, l->batch*l->outputs*sizeof(real));
+        l->x_norm  = realloc(l->x_norm, l->batch*l->outputs*sizeof(real));
     }
 
 #ifdef GPU
@@ -230,9 +230,9 @@ void forward_deconvolutional_layer(const layer l, network net)
     fill_cpu(l.outputs*l.batch, 0, l.output, 1);
 
     for(i = 0; i < l.batch; ++i){
-        float *a = l.weights;
-        float *b = net.input + i*l.c*l.h*l.w;
-        float *c = net.workspace;
+        real *a = l.weights;
+        real *b = net.input + i*l.c*l.h*l.w;
+        real *c = net.workspace;
 
         gemm_cpu(1,0,m,n,k,1,a,m,b,n,0,c,n);
 
@@ -258,16 +258,16 @@ void backward_deconvolutional_layer(layer l, network net)
         backward_bias(l.bias_updates, l.delta, l.batch, l.n, l.out_w*l.out_h);
     }
 
-    //if(net.delta) memset(net.delta, 0, l.batch*l.h*l.w*l.c*sizeof(float));
+    //if(net.delta) memset(net.delta, 0, l.batch*l.h*l.w*l.c*sizeof(real));
 
     for(i = 0; i < l.batch; ++i){
         int m = l.c;
         int n = l.size*l.size*l.n;
         int k = l.h*l.w;
 
-        float *a = net.input + i*m*k;
-        float *b = net.workspace;
-        float *c = l.weight_updates;
+        real *a = net.input + i*m*k;
+        real *b = net.workspace;
+        real *c = l.weight_updates;
 
         im2col_cpu(l.delta + i*l.outputs, l.out_c, l.out_h, l.out_w, 
                 l.size, l.stride, l.pad, b);
@@ -278,9 +278,9 @@ void backward_deconvolutional_layer(layer l, network net)
             int n = l.h*l.w;
             int k = l.size*l.size*l.n;
 
-            float *a = l.weights;
-            float *b = net.workspace;
-            float *c = net.delta + i*n*m;
+            real *a = l.weights;
+            real *b = net.workspace;
+            real *c = net.delta + i*n*m;
 
             gemm_cpu(0,0,m,n,k,1,a,k,b,n,1,c,n);
         }
@@ -289,9 +289,9 @@ void backward_deconvolutional_layer(layer l, network net)
 
 void update_deconvolutional_layer(layer l, update_args a)
 {
-    float learning_rate = a.learning_rate*l.learning_rate_scale;
-    float momentum = a.momentum;
-    float decay = a.decay;
+    real learning_rate = a.learning_rate*l.learning_rate_scale;
+    real momentum = a.momentum;
+    real decay = a.decay;
     int batch = a.batch;
 
     int size = l.size*l.size*l.c*l.n;
