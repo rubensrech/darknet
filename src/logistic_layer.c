a3cdec3d25ba56b110d4ab950f0bebb8c496caec
Rubens Rech <rubensch@icloud.com>
2018-11-19 14:01:57 -0200

    Change logistic_layer

--- src/logistic_layer.c
+++ src/logistic_layer.c
@@ -17,10 +17,10 @@ layer make_logistic_layer(int batch, int inputs)
     l.batch = batch;
     l.inputs = inputs;
     l.outputs = inputs;
-    l.loss = calloc(inputs*batch, sizeof(float));
-    l.output = calloc(inputs*batch, sizeof(float));
-    l.delta = calloc(inputs*batch, sizeof(float));
-    l.cost = calloc(1, sizeof(float));
+    l.loss = calloc(inputs*batch, sizeof(real));
+    l.output = calloc(inputs*batch, sizeof(real));
+    l.delta = calloc(inputs*batch, sizeof(real));
+    l.cost = calloc(1, sizeof(real));
 
     l.forward = forward_logistic_layer;
     l.backward = backward_logistic_layer;
