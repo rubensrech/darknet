1dabdc77b45c47c13ed132ba7da54041ae4aab24
Rubens Rech <rubensch@icloud.com>
2018-11-19 00:56:19 -0200

    Change activations

--- src/activations.c
+++ src/activations.c
@@ -62,7 +62,7 @@ ACTIVATION get_activation(char *s)
     return RELU;
 }
 
-float activate(float x, ACTIVATION a)
+real activate(real x, ACTIVATION a)
 {
     switch(a){
         case LINEAR:
@@ -97,7 +97,7 @@ float activate(float x, ACTIVATION a)
     return 0;
 }
 
-void activate_array(float *x, const int n, const ACTIVATION a)
+void activate_array(real *x, const int n, const ACTIVATION a)
 {
     int i;
     for(i = 0; i < n; ++i){
@@ -105,7 +105,7 @@ void activate_array(float *x, const int n, const ACTIVATION a)
     }
 }
 
-float gradient(float x, ACTIVATION a)
+real gradient(real x, ACTIVATION a)
 {
     switch(a){
         case LINEAR:
@@ -140,7 +140,7 @@ float gradient(float x, ACTIVATION a)
     return 0;
 }
 
-void gradient_array(const float *x, const int n, const ACTIVATION a, float *delta)
+void gradient_array(const real *x, const int n, const ACTIVATION a, real *delta)
 {
     int i;
     for(i = 0; i < n; ++i){
