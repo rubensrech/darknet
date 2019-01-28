5b580e08f342053313289c4e1428d3568477c303
Rubens Rech <rubensch@icloud.com>
2019-01-28 13:48:07 -0200

    Maxpool layer was missing

--- src/maxpool_layer.c
+++ src/maxpool_layer.c
@@ -20,7 +20,7 @@ image get_maxpool_delta(maxpool_layer l)
 
 maxpool_layer make_maxpool_layer(int batch, int h, int w, int c, int size, int stride, int padding)
 {
-    maxpool_layer l = {0};
+    maxpool_layer l = {};
     l.type = MAXPOOL;
     l.batch = batch;
     l.h = h;
@@ -35,9 +35,9 @@ maxpool_layer make_maxpool_layer(int batch, int h, int w, int c, int size, int s
     l.size = size;
     l.stride = stride;
     int output_size = l.out_h * l.out_w * l.out_c * batch;
-    l.indexes = calloc(output_size, sizeof(int));
-    l.output =  calloc(output_size, sizeof(real));
-    l.delta =   calloc(output_size, sizeof(real));
+    l.indexes = (int*)calloc(output_size, sizeof(int));
+    l.output = (real*)calloc(output_size, sizeof(real));
+    l.delta = (real*)calloc(output_size, sizeof(real));
     l.forward = forward_maxpool_layer;
     l.backward = backward_maxpool_layer;
     #ifdef GPU
@@ -62,9 +62,9 @@ void resize_maxpool_layer(maxpool_layer *l, int w, int h)
     l->outputs = l->out_w * l->out_h * l->c;
     int output_size = l->outputs * l->batch;
 
-    l->indexes = realloc(l->indexes, output_size * sizeof(int));
-    l->output = realloc(l->output, output_size * sizeof(real));
-    l->delta = realloc(l->delta, output_size * sizeof(real));
+    l->indexes = (int*)realloc(l->indexes, output_size * sizeof(int));
+    l->output = (real*)realloc(l->output, output_size * sizeof(real));
+    l->delta = (real*)realloc(l->delta, output_size * sizeof(real));
 
     #ifdef GPU
     cuda_free((real *)l->indexes_gpu);
