6efb276441d0d523ff8d206138508c6022a6d7f6
Rubens Rech <rubensch@icloud.com>
2019-02-05 13:10:58 -0200

    Fix softmax_layer.c to support half

--- src/softmax_layer.c
+++ src/softmax_layer.c
@@ -58,7 +58,7 @@ void forward_softmax_layer(const softmax_layer l, network net)
 
 void backward_softmax_layer(const softmax_layer l, network net)
 {
-    axpy_cpu(l.inputs*l.batch, 1, l.delta, 1, net.delta, 1);
+    axpy_cpu(l.inputs*l.batch, CAST(1), l.delta, 1, net.delta, 1);
 }
 
 #ifdef GPU
