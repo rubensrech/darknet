96f7fff4a1812fa32a5ad28be7f9d82c9389ad19
Rubens Rech <rubensch@icloud.com>
2019-02-14 13:12:16 -0200

    Fix dropout_layer_kernels.cu to support half

--- src/dropout_layer_kernels.cu
+++ src/dropout_layer_kernels.cu
@@ -8,10 +8,10 @@
 #include "utils.h"
 // }
 
-__global__ void yoloswag420blazeit360noscope(real *input, int size, real *rand, real prob, real scale)
+__global__ void yoloswag420blazeit360noscope(real_device *input, int size, real_device *rand, real_device prob, real_device scale)
 {
     int id = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
-    if(id < size) input[id] = (rand[id] < prob) ? 0 : input[id]*scale;
+    if(id < size) input[id] = (rand[id] < prob) ? CAST_DEV(0) : input[id]*scale;
 }
 
 void forward_dropout_layer_gpu(dropout_layer layer, network net)
@@ -27,7 +27,7 @@ void forward_dropout_layer_gpu(dropout_layer layer, network net)
     cuda_push_array(layer.rand_gpu, layer.rand, size);
     */
 
-    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK>>>(net.input_gpu, size, layer.rand_gpu, layer.probability, layer.scale);
+    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK>>>((real_device*)net.input_gpu, size, (real_device*)layer.rand_gpu, (real_device)layer.probability, (real_device)layer.scale);
     check_error(cudaPeekAtLastError());
 }
 
@@ -36,6 +36,6 @@ void backward_dropout_layer_gpu(dropout_layer layer, network net)
     if(!net.delta_gpu) return;
     int size = layer.inputs*layer.batch;
 
-    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK>>>(net.delta_gpu, size, layer.rand_gpu, layer.probability, layer.scale);
+    yoloswag420blazeit360noscope<<<cuda_gridsize(size), BLOCK>>>((real_device*)net.delta_gpu, size, (real_device*)layer.rand_gpu, (real_device)layer.probability, (real_device)layer.scale);
     check_error(cudaPeekAtLastError());
 }
