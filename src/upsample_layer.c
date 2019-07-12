61755fb7f9ef143ffa1c02b4b194d982002ecd84
Rubens Rech <rubensch@icloud.com>
2019-07-12 15:34:47 -0300

    Fix MIX PRECISION on upsample layers

--- src/upsample_layer.c
+++ src/upsample_layer.c
@@ -120,14 +120,14 @@ void forward_upsample_layer_gpu(const layer l, network net) {
         upsample_gpu(net.input_gpu, l.w, l.h, l.c, l.batch, l.stride, 1, l.scale, l.output_gpu);
 }
 void forward_upsample_layer_float_gpu(const layer l, network net) {
-    fill_gpu(l.outputs*l.batch, 0, l.output_gpu, 1);
+    fill_gpu(l.outputs*l.batch, 0, l.output_float_gpu, 1);
     if (l.reverse)
         upsample_gpu(l.output_float_gpu, l.out_w, l.out_h, l.c, l.batch, l.stride, 0, l.scale, net.input_float_gpu);
     else
         upsample_gpu(net.input_float_gpu, l.w, l.h, l.c, l.batch, l.stride, 1, l.scale, l.output_float_gpu);
 }
 void forward_upsample_layer_half_gpu(const layer l, network net) {
-    fill_gpu(l.outputs*l.batch, 0, l.output_gpu, 1);
+    fill_gpu(l.outputs*l.batch, 0, l.output_half_gpu, 1);
     if (l.reverse)
         upsample_gpu(l.output_half_gpu, l.out_w, l.out_h, l.c, l.batch, l.stride, 0, l.scale, net.input_half_gpu);
     else
